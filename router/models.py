e038bcef290a1a9ddf9a14145180b88a22aed05d
Mohanad Kaleia <ms.kaleia@gmail.com>
2018-08-04 13:15:46 -0700

    rebase test master

--- router/models.py
+++ router/models.py
@@ -3,7 +3,7 @@ from django.db import models
 
 class Team(models.Model):
     name = models.CharField(max_length=100)
-
+    #FIXME just delete (master)
 
 class Vehicle(models.Model):
     name = models.CharField(max_length=100)
