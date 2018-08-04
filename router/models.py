13de9bb3cf315799ef30894457c217bb9ba2f678
Mohanad Kaleia <ms.kaleia@gmail.com>
2018-08-04 12:45:00 -0700

    Initial commit

---
+++ router/models.py
@@ -0,0 +1,25 @@
+from django.db import models
+
+
+class Team(models.Model):
+    name = models.CharField(max_length=100)
+
+
+class Vehicle(models.Model):
+    name = models.CharField(max_length=100)
+    team = models.ForienKey(max_length=100)
+    capacity = models.IntegerFIeld()
+    speed = models.IntegerField()
+
+    @classmethod
+    def get_vehicle_number(cls):
+        return len(cls.objects.all())
+
+
+class Task(models.Model):
+    name = models.CharField(max_length=10)
+    latitude = models.DecimalField()
+    longitude = models.DecimalField()
+    min_time_window = models.DateTimeField()
+    max_time_window = models.DateTimeField()
+    vehicle = models.ForienKey(Vehicle)
